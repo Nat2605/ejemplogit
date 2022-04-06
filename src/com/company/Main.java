e0a53589cbace926c620071f30380c53690de974
u_35010610 <Nat2605>
2022-04-06 19:41:27 +0100

    Añadí una suma

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        int num1=3;
+        int num2=6;
+        int suma;
+
+        suma=num1+num2;
+        System.out.println("El resultado de la suma es: " +suma);
     }
 }
